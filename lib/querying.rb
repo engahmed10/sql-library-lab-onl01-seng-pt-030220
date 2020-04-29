29c5bdb7a6900b238afadf13090847f97f43b6ad
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-29 20:14:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,6 +22,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name,count(character_books.book_id) FROM characters INNER JOIN character_books
-  ON characters.id = character_books.character_id GROUP BY character_books.book_id ORDER BY count(character_books.book_id) ASC LIMIT 1;"
+  "SELECT characters.name,count(character_books.book_id) FROM characters INNER JOIN
+  character_books ON characters.id = character_books.character_id GROUP BY 
+  character_books.book_id ORDER BY count(character_books.book_id) ASC LIMIT 1;"
 end
