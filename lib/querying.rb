8a3c26cfde01af3c253a4bb0e1dfbbefad7bee6e
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-29 19:56:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series INNER JOIN authors ON series.author_id = authors.id
-  INNER JOIN characters ON authors.id = characters.author_id  ORDER BY count(characters.species) HAVING characters.species='human';"
+  INNER JOIN characters ON authors.id = characters.author_id  ORDER BY count(characters.species) WHERE characters.species='human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
