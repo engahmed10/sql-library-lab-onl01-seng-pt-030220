a107c556015456be8afaa0b4b91101b57439e098
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-29 17:22:36 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE subgenres (id INTEGER PRIMARY KEY ,name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY ,name TEXT);
 CREATE TABLE Books (id INTEGER PRIMARY KEY ,title TEXT,year INTEGER,series_id INTEGER);
 CREATE TABLE Characters (id INTEGER PRIMARY KEY ,name TEXT,motto TEXT,species TEXT,author_id INTEGER);
+CREATE TABLE character_books 
