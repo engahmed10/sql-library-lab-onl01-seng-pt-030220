ea654a797656a0016b5877b288946ddc6a6cf9d3
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-29 18:25:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  " SELECT characters.species ,count(species) FROM characters HAVING "
+  " SELECT characters.species ,count(species) FROM characters GROUP BY characters.species HAVING "
 end
 
 def select_name_and_series_subgenres_of_authors
