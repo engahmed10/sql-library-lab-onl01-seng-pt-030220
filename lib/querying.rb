a0a55d6eaa67b37b71927122b223fd5fbdeb0912
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-29 20:16:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,6 +23,5 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name,count(character_books.book_id) FROM characters INNER JOIN
-  character_books ON characters.id = character_books.character_id GROUP BY
-  character_books.book_id ORDER BY count(character_books.book_id) DESC;"
+  character_books ON characters.id = character_books.character_id;"
 end
